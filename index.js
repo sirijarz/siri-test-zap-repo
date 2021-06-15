78ff5e43e6c088b2b688510f18f7b9a8fb7a3ccf
sirijarz <sirijarmale@gmail.com>
2021-06-15 19:30:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const {
+  config: authentication,
+  befores = [],
+  afters = [],
+} = require('./authentication');
+
+const repositoryResource = require("./resources/repository");
+
+module.exports = {
+  // This is just shorthand to reference the installed dependencies you have.
+  // Zapier will need to know these before we can upload.
+  version: require('./package.json').version,
+  platformVersion: require('zapier-platform-core').version,
+
+  authentication,
+
+  beforeRequest: [...befores],
+
+  afterResponse: [...afters],
+
+  // If you want your trigger to show up, you better include it here!
+  triggers: {},
+
+  // If you want your searches to show up, you better include it here!
+  searches: {},
+
+  // If you want your creates to show up, you better include it here!
+  creates: {},
+
+  resources: {
+    [repositoryResource.key]: repositoryResource
+  },
+};
